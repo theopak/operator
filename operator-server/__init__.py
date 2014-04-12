23c438b748b9e262918e1917fb6b1768ed19021d
Theodore X. Pak <mail@theopak.com>
2014-04-12 15:18:31 -0400

    Adds skeleton for Flask, PyMongo, Inuit.css, jQuery 1.10.2, jQuery smooth-scroll, jQuery nprogress.

---
+++ operator-server/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request, jsonify, Response
+#import logging
+import json
+from bson import json_util
+from flask.ext.pymongo import PyMongo
+
+
+# Connect to remote MongoDB instance
+app = Flask(__name__, static_folder='static')
+app.config['MONGO_HOST'] = '0.mongolab.com'
+app.config['MONGO_PORT'] = 0
+app.config['MONGO_DBNAME'] = 'dbname'
+app.config['MONGO_USERNAME'] = 'username'
+app.config['MONGO_PASSWORD'] = 'password'
+mongo = PyMongo(app, config_prefix='MONGO')
+
+
+@app.route('/')
+def home_page():
+    '''
+    Home page
+    '''
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+        app.debug = True  # Do not enable Debug mode on the production server!
+        app.run()
