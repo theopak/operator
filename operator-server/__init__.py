22a178b55eb50e3a1a62c683cc4a0f9255344965
Derek Schultz <derek@derekschultz.net>
2014-04-13 00:47:59 -0400

    Adds company info query and fixes unicode issues with json dumping

--- operator-server/__init__.py
+++ operator-server/__init__.py
@@ -11,10 +11,20 @@ def home_page():
     '''
     return render_template('index.html')
 
-@app.route('/companies/<query>.json')
+@app.route('/companies/search/<query>')
 def company_search(query):
-    results = list(mongo.db.companies.find({'name': {'$regex': query, '$options': '-i'}}))
-    return json.dumps(results, default=json_util.default)
+    results = list(mongo.db.companies.find({
+        'name': {
+            '$regex': query,
+            '$options': '-i'
+        }
+    }))
+    return json.dumps(results, default=json_util.default, ensure_ascii=False)
+
+@app.route('/companies/info/<query>')
+def company_info(query):
+    company = mongo.db.companies.find_one({'name': query})
+    return json.dumps(company, default=json_util.default, ensure_ascii=False)
 
 @app.route('/outbound/new', methods=['POST', 'PUT'])
 def handle_outbound():
